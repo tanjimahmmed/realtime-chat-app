179c31682ce83aaa3ceb8635833852c35a081868
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-14 19:59:38 +0600

    add signup controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import { connectDB } from './lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -13,20 +13,20 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); // req.body
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Make ready for deploy
-if(process.env.NODE_ENV ===  'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-    });
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-    console.log('Server running on port: ' + PORT),
-    connectDB();
+  console.log("Server running on port: " + PORT), connectDB();
 });
