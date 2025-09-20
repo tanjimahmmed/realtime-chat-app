ca6eb2bc6acf6fcdaaa1e48a59cbc1f86717dfd0
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-20 17:49:59 +0600

    sidebar completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ import { ENV } from "./lib/env.js";
 const app = express();
 const __dirname = path.resolve();
 
-const PORT =ENV.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
 app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,36 @@
+import {useEffect} from 'react';
+import { useChatStore } from '../store/useChatStore';
+import UsersLoadingSkeleton from './UsersLoadingSkeleton';
+
+function ContactList() {
+  const {getAllContacts, allContacts, setSelectedUser, isUsersLoading} = useChatStore();
+
+  useEffect(() => {
+    getAllContacts();
+  }, [getAllContacts]);
+
+
+  if(isUsersLoading) return <UsersLoadingSkeleton/>;
+
+  return (
+    <div>
+      {allContacts.map((contact) => (
+        <div key={contact._id} 
+        className='bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors' 
+        onClick={()=>setSelectedUser(contact)}>
+          <div className="flex items-center gap-3">
+            {/* Online status with socket */}
+            <div className={`avatar online`}>
+              <div className="size-12 rounded-full">
+                <img src={contact.profilePic || '/avatar.png'} alt={contact.fullName} />
+              </div>
+            </div>
+            <h4 className='text-slate-200 font-medium truncate'>{contact.fullName}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ContactList
